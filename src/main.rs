f43f5e518cf4175d972ba2fceaca1c895f3be932
Mark <67615357+Dummi26@users.noreply.github.com>
2023-03-14 09:19:58 +0100

    script file has to be provided as the first arg

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     .into();
     assert!(val.fits_in(&case).is_empty());
     let script = parse::parse::parse(&mut parse::file::File::new(
-        std::fs::read_to_string("/tmp/script.txt").unwrap(),
+        std::fs::(std::env::args().nth(1).unwrap()).unwrap(),
     ))
     .unwrap();
     println!(" - - - - -");
