2acdcd3f53bb1b9da11f169399082617e5e30ac5
Dummi26 <markbaumeister26@gmail.com>
2023-04-12 22:23:07 +0200

    moved Cargo.toml and src/ to /mers/Cargo.toml and /mers/src/ because rust-analyzer was apparently very confused when I was trying to edit projects in mers_libs/*/.

--- mers/src/libs/path.rs
+++ mers/src/libs/path.rs
@@ -5,9 +5,16 @@ pub fn path_from_string(path: &str, script_directory: &PathBuf) -> Option<PathBu
     if path.is_absolute() {
         return Some(path);
     }
-    let p = script_directory.join(&path);
-    if p.exists() {
-        return Some(p);
+    if let Some(p) = script_directory
+        .canonicalize()
+        .unwrap_or_else(|_| script_directory.clone())
+        .parent()
+    {
+        eprintln!("Parent: {:?}", p);
+        let p = p.join(&path);
+        if p.exists() {
+            return Some(p);
+        }
     }
     if let Ok(mers_lib_dir) = std::env::var("MERS_LIB_DIR") {
         let p = PathBuf::from(mers_lib_dir).join(&path);
